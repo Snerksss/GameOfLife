4df237ac052db81bc76f21ca3a5f2e406a96a537
Snerksss <simon.berndt@t-online.de>
2022-07-10 09:23:38 +0200

    renew of frameGenerator

--- GameOfLifeForms/Form1.Designer.cs
+++ GameOfLifeForms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameOfLifeForms
 {
-    partial class Form1
+    partial class GameField
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,40 +29,45 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numberOfPixels = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPixels)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 3, 12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(780, 780);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // numericUpDown1
+            // numberOfPixels
             // 
-            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.numericUpDown1.Location = new System.Drawing.Point(72, 807);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.numberOfPixels.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numberOfPixels.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numberOfPixels.Location = new System.Drawing.Point(73, 841);
+            this.numberOfPixels.Name = "numberOfPixels";
+            this.numberOfPixels.Size = new System.Drawing.Size(120, 23);
+            this.numberOfPixels.TabIndex = 1;
+            this.numberOfPixels.Value = new decimal(new int[] {
             50,
             0,
             0,
             0});
+            this.numberOfPixels.ValueChanged += new System.EventHandler(this.numberOfPixels_ValueChanged);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 809);
+            this.label1.Location = new System.Drawing.Point(35, 843);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 15);
             this.label1.TabIndex = 2;
@@ -70,41 +75,47 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.Color.LimeGreen;
             this.button1.Font = new System.Drawing.Font("Showcard Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(701, 809);
+            this.button1.Location = new System.Drawing.Point(717, 822);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 47);
             this.button1.TabIndex = 3;
             this.button1.Text = "GO";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.BackColor = System.Drawing.Color.Red;
             this.button2.Font = new System.Drawing.Font("Showcard Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(620, 809);
+            this.button2.Location = new System.Drawing.Point(636, 822);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 47);
             this.button2.TabIndex = 4;
             this.button2.Text = "STOP";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.BackColor = System.Drawing.Color.Blue;
             this.button3.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(509, 809);
+            this.button3.Location = new System.Drawing.Point(511, 825);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 47);
             this.button3.TabIndex = 5;
             this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // Form1
+            // GameField
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -113,12 +124,14 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.numberOfPixels);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "GameField";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Game Of Life";
+            this.Load += new System.EventHandler(this.GameField_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfPixels)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,7 +140,7 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private NumericUpDown numericUpDown1;
+        private NumericUpDown numberOfPixels;
         private Label label1;
         private Button button1;
         private Button button2;
--- GameOfLifeForms/Form1.cs
+++ GameOfLifeForms/Form1.cs
@@ -1,10 +1,109 @@
 namespace GameOfLifeForms
 {
-    public partial class Form1 : Form
+    public partial class GameField : Form
     {
-        public Form1()
+        int[,] gameField;
+        int pixelSize;
+        Boolean run;
+        public GameField()
         {
             InitializeComponent();
+            run = false;
+        }
+
+        private void GameField_Load(object sender, EventArgs e)
+        {
+            resizeField();
+            
+        }
+
+        
+
+        private void numberOfPixels_ValueChanged(object sender, EventArgs e)
+        {
+            resizeField();
+            
+        }
+
+        private void resizeField()
+        {
+            int pixelCount = Decimal.ToInt32(numberOfPixels.Value);
+            int widthHeight = pictureBox1.Height;
+            int distance = widthHeight % pixelCount;
+            int additionalSize = pixelCount - distance;
+            if (distance > pixelCount / 2)
+            {
+                additionalSize = pixelCount - distance;
+            }
+            else
+            {
+                additionalSize = - distance;
+            }
+            pictureBox1.Height = pictureBox1.Width = pictureBox1.Height + additionalSize;
+            this.Size = new Size(this.Size.Width + additionalSize, this.Size.Height + additionalSize);
+            this.Location = new Point(5, 10);
+            this.pixelSize = pictureBox1.Height / Decimal.ToInt32(numberOfPixels.Value);
+            gameField = new int[Decimal.ToInt32(numberOfPixels.Value), Decimal.ToInt32(numberOfPixels.Value)];
+            
+            frameGenerator();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = numberOfPixels.Visible = false;
+
+            button3.BackColor = Color.Gray;
+            button3.IsAccessible = false;
+            button3.Enabled = false;
+
+            run = true;
+
+            gameField[1, 1] = 1;
+
+            gameField[20, 4] = 1;
+
+            frameGenerator();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.Blue;
+            button3.Enabled = true;
+            run = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(run == false)
+            {
+                label1.Visible = numberOfPixels.Visible = true;
+            }
+        }
+
+        private void frameGenerator()
+        {
+            using (Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height))
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (SolidBrush cellBrush = new SolidBrush(Color.DarkOrange))
+            {
+                g.Clear(Color.Black);
+                for(int i = 0; i < numberOfPixels.Value; i++)
+                {
+                    for(int j = 0; j < numberOfPixels.Value; j++)
+                    {
+                        if(gameField[i, j] != 0)
+                        {
+                            g.FillRectangle(cellBrush, new Rectangle(this.pixelSize * i, this.pixelSize * j, this.pixelSize, this.pixelSize));
+                        }
+                    }
+                }
+                pictureBox1.Image = (Bitmap)bmp.Clone();
+            }
         }
     }
 }
